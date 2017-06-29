a62a25581b2d4375ba8931a27994dce05293652b
Erwin de Reus <erwin3007@gmail.com>
2017-06-29 15:28:52 +0200

    Forms appie

---
+++ Erwin/Foodmeister/Foodmeister/Browse_menu.Designer.cs
@@ -0,0 +1,161 @@
+﻿using System;
+
+namespace Foodmeister
+{
+    partial class Browse_menu
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Browse_menu));
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.Browse = new System.Windows.Forms.Button();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.Logout = new System.Windows.Forms.Button();
+            this.Bookmarks = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.SystemColors.Highlight;
+            this.pictureBox1.Location = new System.Drawing.Point(0, 0);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(1920, 205);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.SystemColors.Highlight;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 32F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.label1.Location = new System.Drawing.Point(137, 48);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(394, 51);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Browse All Recipes";
+            // 
+            // Browse
+            // 
+            this.Browse.BackColor = System.Drawing.SystemColors.HotTrack;
+            this.Browse.FlatAppearance.BorderSize = 0;
+            this.Browse.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.Browse.Font = new System.Drawing.Font("Microsoft Sans Serif", 32F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Browse.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.Browse.Location = new System.Drawing.Point(0, 125);
+            this.Browse.Name = "Browse";
+            this.Browse.Size = new System.Drawing.Size(640, 80);
+            this.Browse.TabIndex = 2;
+            this.Browse.TabStop = false;
+            this.Browse.Text = "Browse";
+            this.Browse.UseVisualStyleBackColor = false;
+            this.Browse.Click += new System.EventHandler(this.Browse_Click);
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.BackColor = System.Drawing.SystemColors.Highlight;
+            this.pictureBox2.ErrorImage = ((System.Drawing.Image)(resources.GetObject("pictureBox2.ErrorImage")));
+            this.pictureBox2.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox2.Image")));
+            this.pictureBox2.InitialImage = ((System.Drawing.Image)(resources.GetObject("pictureBox2.InitialImage")));
+            this.pictureBox2.Location = new System.Drawing.Point(32, 30);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(80, 80);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox2.TabIndex = 3;
+            this.pictureBox2.TabStop = false;
+            // 
+            // Logout
+            // 
+            this.Logout.BackColor = System.Drawing.SystemColors.Highlight;
+            this.Logout.FlatAppearance.BorderSize = 0;
+            this.Logout.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.Logout.Font = new System.Drawing.Font("Microsoft Sans Serif", 32F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Logout.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.Logout.Location = new System.Drawing.Point(1280, 125);
+            this.Logout.Name = "Logout";
+            this.Logout.Size = new System.Drawing.Size(640, 80);
+            this.Logout.TabIndex = 5;
+            this.Logout.TabStop = false;
+            this.Logout.Text = "Logout";
+            this.Logout.UseVisualStyleBackColor = false;
+            this.Logout.Click += new System.EventHandler(this.Logout_Click);
+            // 
+            // Bookmarks
+            // 
+            this.Bookmarks.BackColor = System.Drawing.SystemColors.Highlight;
+            this.Bookmarks.FlatAppearance.BorderSize = 0;
+            this.Bookmarks.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.Bookmarks.Font = new System.Drawing.Font("Microsoft Sans Serif", 32F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Bookmarks.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.Bookmarks.Location = new System.Drawing.Point(640, 125);
+            this.Bookmarks.Name = "Bookmarks";
+            this.Bookmarks.Size = new System.Drawing.Size(640, 80);
+            this.Bookmarks.TabIndex = 6;
+            this.Bookmarks.TabStop = false;
+            this.Bookmarks.Text = "My Bookmarks";
+            this.Bookmarks.UseVisualStyleBackColor = false;
+            this.Bookmarks.Click += new System.EventHandler(this.Bookmarks_Click);
+            // 
+            // Browse_menu
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoValidate = System.Windows.Forms.AutoValidate.EnablePreventFocusChange;
+            this.BackColor = System.Drawing.SystemColors.Menu;
+            this.ClientSize = new System.Drawing.Size(1904, 1041);
+            this.Controls.Add(this.Bookmarks);
+            this.Controls.Add(this.Logout);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.Browse);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Browse_menu";
+            this.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.Text = "Foodmeister";
+            this.Load += new System.EventHandler(this.Browse_menu_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        public System.Windows.Forms.PictureBox pictureBox1;
+        public System.Windows.Forms.Label label1;
+        public System.Windows.Forms.Button Browse;
+        public System.Windows.Forms.PictureBox pictureBox2;
+        public System.Windows.Forms.Button Logout;
+        public System.Windows.Forms.Button Bookmarks;
+    }
+}
+
